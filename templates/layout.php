0b51d34302e50f54627c9ee2ea79833312444ee2
Lennore90 <lennore_90@mail.ru>
2017-11-28 00:18:08 +0300

    Changes before checkout branch

--- templates/layout.php
+++ templates/layout.php
@@ -42,7 +42,7 @@
                 <nav class="main-navigation">
                     <ul class="main-navigation__list">
                         <?php $project_count = 0;?>
-                        <?php foreach ($project_cats as $project_name):?>
+                        <?php foreach ($project_cats as $key =>  $project_name):?>
                             <?php
                             $active_class = '';
                             if ($project_count === 0) {
