e24a516612409e28809f3ca064ed173491444eb4
Lennore90 <lennore_90@mail.ru>
2017-12-01 16:40:10 +0300

    неотслеживаемая папка из статуса

---
+++ 417449-doingsdone/index.php
@@ -0,0 +1,47 @@
+<?php
+
+
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+
+// устанавливаем часовой пояс в Московское время
+date_default_timezone_set('Europe/Moscow');
+
+require_once('functions.php');
+require_once('data.php');
+
+$task_list = [];
+	if (isset($_GET['project_id'])) {
+		$index=$_GET['project_id'];
+		if (array_key_exists($index, $project_cats)) {
+			$cat=$project_cats[$index];
+			if ($cat== 'Все'){
+					$task_list=$array_tasks;
+				} else {
+					foreach ($array_tasks as $task) {
+						if ($task['task_category']== $cat) {
+						$task_list[]=$task;
+					} 
+				}
+			}
+		}else {
+			http_response_code(404);
+		}
+	} else {
+		$task_list = $array_tasks;
+	}
+
+$content = renderTemplate('templates/index.php', [
+    'show_complete_tasks' => $show_complete_tasks,
+    'array_tasks' => $task_list,
+]);
+
+$page_layout = renderTemplate('templates/layout.php', [
+    'page_title' => 'Дела в порядке', 
+    'page_content' => $content,
+    'project_cats' => $project_cats,
+    'array_tasks' => $array_tasks,
+]);
+
+print ($page_layout);
+?>
